cb88a6b1f27cd3242e94189afd79a14cd22abc8d
Arno Pan <apan11@my.bcit.ca>
2025-03-05 18:32:10 -0800

    Added task function to js. Added more getElementById.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,23 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTaskButton');
+
 const list = document.getElementById('list');
 const doneList = document.getElementById('done-list');
 
+
 let draggedItem = null;
 
+// Function: Add Task
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if(taskText !== ''){
+        const newTask = document.createElement('li');
+        newTask.textContent = taskText;
+        newTask.classList.add('item');
+        newTask.setAttribute('draggable', 'true');
+        list.appendChild(newTask);
+        taskInput.value = ''; // clear after 
+    }
+}
+
+// Event Listener for Add Task Button
